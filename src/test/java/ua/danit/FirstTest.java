a282772d32f9efdac5a980b9a7367e40c789b0c4
Max Romanenko <romanenkomax.ua@gmail.com>
2018-06-02 16:25:08 +0300

    Improved userMail

--- src/test/java/ua/danit/FirstTest.java
+++ src/test/java/ua/danit/FirstTest.java
@@ -35,6 +35,17 @@ public class FirstTest {
         WebElement profileUser = driver.findElement(By.className("sn_menu_title"));
         String mailUser = profileUser.getText();
         Assert.assertEquals("kiev33@i.ua", mailUser);
+        WebElement newLetter = driver.findElement(By.linkText("Создать письмо"));
+        newLetter.click();
+        WebElement toField = driver.findElement(By.id("to"));
+        toField.sendKeys("romanenkomax.ua@gmail.com");
+        WebElement subjectField = driver.findElement(By.xpath("/html/body/div[4]/div[5]/div[1]/div[1]/div[1]/div/form/div[5]/div[2]/span/input[1]"));
+        subjectField.sendKeys("Test e-mail!");
+        WebElement textField = driver.findElement(By.id("text"));
+        textField.sendKeys("This is the text e-mail!!!");
+        WebElement sendButton = driver.findElement(By.xpath("/html/body/div[4]/div[5]/div[1]/div[1]/p[3]/input[1]"));
+        sendButton.click();
+
     }
 
     @AfterClass
